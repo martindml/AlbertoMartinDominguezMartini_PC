522cbdb5f7bb854fee3d9bd7708245423fecd68a
martindml <martin.dominguezm@mercadolibre.com>
2021-05-26 23:10:29 -0300

    Clase de tarde

--- Meli_setup/src/meli/ejerc/RadixSortEjerc.java
+++ Meli_setup/src/meli/ejerc/RadixSortEjerc.java
@@ -1,16 +1,49 @@
 package meli.ejerc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class RadixSortEjerc
 {
-	public static void radixSort(int []arr)
+	public static int[] radixSort(int []arr)
 	{
 		// PROGRAMAR AQUI
+		String[] sArray = StringUtil.toStringArray(arr);
+
+		//Limpio el arrayList
+		arr = null;
+
+		StringUtil.lNormalize(sArray,'0');
+
+		HashMap<String,  ArrayList<String>> hashMap = StringUtil.crearHashMap();
+
+		int largoDelNumero = sArray[0].length();
+
+		int iteracion = 0;
+
+		while(largoDelNumero > 0)
+		{
+			largoDelNumero = largoDelNumero -1;
+
+			for(String item:sArray)
+			{
+				hashMap = StringUtil.agregarAlHashMap(hashMap,item,largoDelNumero);
+			}
+
+			iteracion = iteracion +1;
+			//Vuelvo a cargar el arrayList con el nuevo orden
+			sArray = StringUtil.cargarArrayListConNuevoOrden(hashMap,sArray,iteracion);
+
+			hashMap = StringUtil.limpiarHashMap(hashMap);
+		}
+
+		return StringUtil.toIntArray(sArray);
 	}
 
 	public static void main(String[] args)
 	{
 		int arr[]={16223,898,13,906,235,23,9,1532,6388,2511,8};
-		radixSort(arr);
+		arr = radixSort(arr);
 		
 		for(int i=0; i<arr.length;i++)
 		{
--- Meli_setup/src/meli/ejerc/StringUtil.java
+++ Meli_setup/src/meli/ejerc/StringUtil.java
@@ -1,12 +1,23 @@
 package meli.ejerc;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class StringUtil
 {
 	// Retorna una cadena compuesta por n caracteres c
 	// Ejemplo: replicate('x',5) ==> 'xxxxx'
 	public static String replicate(char c,int n)
 	{
-		return "";
+		String resultado = "";
+
+		for(int i = 0; i < n; i++ )
+		{
+			resultado = resultado + c;
+		}
+
+		return resultado;
 	}
 
 	// Retorna una cadena de longitud n, compuesta por s
@@ -15,33 +26,152 @@ public class StringUtil
 	// Ejemplo lpad("5",3,'0') ==> "005"
 	public static String lpad(String s, int n, char c)
 	{
-		return "";
+		String resultado = s;
+
+		String mask = replicate('0',n - s.length());
+
+		return mask + resultado;
 	}
 
 	// Retorna un String[] conteniendo los elementos de arr
 	// representados como cadenas de caracteres
 	public static String[] toStringArray(int arr[])
 	{
-		return null;
+		String[] resultado = new String[arr.length];
+
+		for(int i=0; i < arr.length; i++)
+		{
+			resultado[i] = Integer.toString(arr[i]);
+		}
+
+		return resultado;
 	}
 
 	// Retorna un String[] conteniendo los elementos de arr
 	// representados como cadenas de caracteres
 	public static int[] toIntArray(String arr[])
 	{
-		return null;
+		int[] resultado = new int[arr.length];
+
+		for(int i=0; i < arr.length; i++)
+		{
+			resultado[i] = Integer.parseInt(arr[i]);
+		}
+
+		return resultado;
 	}
 
 	// Retorna la longitud del elemento con mayor cantidad
 	// de caracteres del array arr
 	public static int maxLength(String arr[])
 	{
-		return 0;
+		int cantidadCaracteres = 0;
+
+		for(int i=0; i < arr.length; i++)
+		{
+			if(cantidadCaracteres < arr[i].length())
+			{
+				cantidadCaracteres = arr[i].length();
+			}
+		}
+
+		return cantidadCaracteres;
 	}
 
 	// Completa los elemento del arr agregando caracteres c
 	// a la izquierda, dejando a todos con la longitud del mayor
 	public static void lNormalize(String arr[], char c)
 	{
+		int longitudMayor =  maxLength(arr);
+
+		for(int i=0; i < arr.length; i++)
+		{
+			arr[i] = lpad(arr[i], longitudMayor , c);
+		}
+	}
+
+	/**
+	 * Metodo que se encarga de crear un nuevo hashMap
+	 * */
+	public static HashMap<String, ArrayList<String>> crearHashMap()
+	{
+		HashMap<String,  ArrayList<String>> hashMap = new HashMap<>();
+
+		hashMap.put("L0",null);
+		hashMap.put("L1",null);
+		hashMap.put("L2",null);
+		hashMap.put("L3",null);
+		hashMap.put("L4",null);
+		hashMap.put("L5",null);
+		hashMap.put("L6",null);
+		hashMap.put("L7",null);
+		hashMap.put("L8",null);
+		hashMap.put("L9",null);
+
+		return hashMap;
+	}
+
+
+	/*
+	*Metodo que se encarga de agregar un valor al hashmap.
+	* Lo hace inteligentemente fijandose la posicion que se envia por parametro por ejemplo
+	* numero = 002, posicion 1
+	* sabe que lo va a insertar al hashmap en la key L0
+	* */
+	public static HashMap <String,  ArrayList<String>> agregarAlHashMap(HashMap <String,  ArrayList<String>> hashMap, String numero, int posicion)
+	{
+		String key = "L" + numero.charAt(posicion);
+
+		ArrayList<String> sList = hashMap.get(key);
+
+		if(hashMap.get(key) == null )
+		{
+			hashMap.put(key, new ArrayList<String>());
+		}
+
+		hashMap.get(key).add(numero);
+
+		return hashMap;
+	}
+
+	/*
+	*Se encarga de limpiar el hashMap
+	* */
+	public static HashMap <String,  ArrayList<String>> limpiarHashMap(HashMap <String,  ArrayList<String>> hashMap)
+	{
+		for(HashMap.Entry<String,ArrayList<String>> item:hashMap.entrySet())
+		{
+			item.setValue(null);
+		}
+
+		return hashMap;
+	}
+
+	/*
+	*Se encarga de cargar el arrayList con el nuevo orden que esta en el HashMap
+	* */
+	public static String[] cargarArrayListConNuevoOrden(HashMap <String,  ArrayList<String>> hashMap, String[] lista, int iteracion)
+	{
+		lista = new String[lista.length];
+
+		String numerosDeLaIteracion = "";
+
+		int position = 0;
+
+		for(HashMap.Entry<String,ArrayList<String>> item:hashMap.entrySet())
+		{
+			if(item.getValue() != null)
+			{
+				for(String numero:item.getValue())
+				{
+					lista[position] = numero;
+					position++;
+					numerosDeLaIteracion = numerosDeLaIteracion + " " + numero + ", ";
+				}
+			}
+		}
+
+		System.out.println("Iteracion de ordenamiento numero: " + iteracion + ", numeros ordenados en esta iteracion:" + numerosDeLaIteracion);
+		return lista;
 	}
 }
